abc4fe82a66af836a40f1a4fe1f49a03fe628754
rxrossi <rx.rossi@gmail.com>
2018-01-04 18:36:08 +0000

    entities reducer now understands GET_SUCCESS action

--- src/reducer/entities/index.js
+++ src/reducer/entities/index.js
@@ -1,5 +1,12 @@
+import * as CONSTANTS from '../../actions/constants';
+
 export default (state = [], action) => {
   switch (action.type) {
+    case CONSTANTS.GET_SUCCESS:
+      return [
+        action.payload,
+        ...state,
+      ];
     default:
       return state;
   }
--- src/reducer/entities/index.test.js
+++ src/reducer/entities/index.test.js
@@ -1,4 +1,7 @@
 import sut from './index';
+import configureSyncActions from '../../actions/syncActionCreators';
+
+const syncActions = configureSyncActions();
 
 const defaultState = [];
 
@@ -9,5 +12,18 @@ describe('entities reducer', () => {
 
     // Assert
     expect(actual).toEqual(defaultState);
+  });
+
+  it('works when receiving a getSucess', () => {
+    // Prepare
+    const payload = { name: 'name' };
+    const action = syncActions.getSuccess(payload);
+
+    // Act
+    const actual = sut(undefined, action);
+
+    // Assert
+    const expected = [payload];
+    expect(actual).toEqual(expected);
   });
 });
