0106ac73663649352521ed93ee8e23b23f741bed
rxrossi <rx.rossi@gmail.com>
2018-01-06 03:18:29 +0000

    Wrote tests related to formActions appendField

--- acceptance_tests/formFieldsActions.test.js
+++ acceptance_tests/formFieldsActions.test.js
@@ -5,7 +5,7 @@ import reusableCRUDRedux from '../src';
 const URL = 'http://localhost:5001/clients';
 
 describe('formFieldsActions', () => {
-  describe('chnage fields', () => {
+  describe('change fields', () => {
     it('can change a regular field', () => {
       // Prepare
       const {
@@ -56,6 +56,66 @@ describe('formFieldsActions', () => {
         ],
       };
       expect(store.getState().clients.formFields.update).toEqual(expected);
+    });
+  });
+  describe('append and remove fields', () => {
+    it('can append a field when formFields.create is empty', () => {
+      // Prepare
+      const {
+        reducer,
+        createFormFieldActions,
+      } = reusableCRUDRedux(URL, 'clients');
+
+      const store = createStore(
+        combineReducers({
+          clients: reducer,
+        }),
+        applyMiddleware(thunk),
+      );
+
+      const action = createFormFieldActions.appendField('sons');
+      // Act
+      store.dispatch(action);
+      // Assert
+      const expected = { sons: [{}] };
+      expect(store.getState().clients.formFields.create).toEqual(expected);
+    });
+
+    it('can append a field when formFields.create is not empty', () => {
+      // Prepare
+      const {
+        reducer,
+        createFormFieldActions,
+      } = reusableCRUDRedux(URL, 'clients');
+
+      const initialState = {
+        clients: {
+          ...reducer(undefined, {}),
+          formFields: {
+            create: {
+              name: 'someName',
+            },
+          },
+        },
+      };
+
+      const store = createStore(
+        combineReducers({
+          clients: reducer,
+        }),
+        initialState,
+        applyMiddleware(thunk),
+      );
+
+      const action = createFormFieldActions.appendField('sons');
+      // Act
+      store.dispatch(action);
+      // Assert
+      const expected = {
+        ...initialState.clients.formFields.create,
+        sons: [{}],
+      };
+      expect(store.getState().clients.formFields.create).toEqual(expected);
     });
   });
 });
--- src/actions/formActions.js
+++ src/actions/formActions.js
@@ -7,6 +7,21 @@ export const changeField = (uniquePairID, formName) => (path = '', value) => ({
   ...uniquePairID,
 });
 
+export const appendField = (uniquePairID, formName) => (path = '', value = {}) => ({
+  type: types[`APPEND_${formName.toUpperCase()}_FIELD`],
+  value,
+  path,
+  ...uniquePairID,
+});
+
+export const removeField = (uniquePairID, formName) => (path = '') => ({
+  type: types[`REMOVE_${formName.toUpperCase()}_FIELD`],
+  path,
+  ...uniquePairID,
+});
+
 export default uniquePairID => formName => ({
   changeField: changeField(uniquePairID, formName),
+  appendField: appendField(uniquePairID, formName),
+  removeField: removeField(uniquePairID, formName),
 });
